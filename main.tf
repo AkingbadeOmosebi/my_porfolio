7912c6a23cb410653038f8d7db483530150b2dfd
AkingbadeOmosebi <akingbadeomosebi@gmail.com>
2025-07-08 19:56:19 +0200

    refactored my code to units

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-# Refactoring my code into smaller units now.
+#This is your standard provider block from registry.terraform docs
 
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "6.2.0"
     }
   }
