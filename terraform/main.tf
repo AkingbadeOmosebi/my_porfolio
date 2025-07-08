7191c6d27c61fe0b34e27ebaa5e4f3ee5277d950
AkingbadeOmosebi <akingbadeomosebi@gmail.com>
2025-07-08 17:45:00 +0200

    # This is my Policy Attachment role, you can find this within the registry docs and read more about it then modify its attributes and apply it here as i did.

--- terraform/main.tf
+++ terraform/main.tf
@@ -230,3 +230,11 @@ resource "aws_iam_role" "ecs_task_execution_role" {
     name = "ecs-task-execution-role"
   }
 }
+
+
+# This is my Policy Attachment role, you can find this within the registry docs and read more about it then modify its attributes and apply it here as i did.
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
